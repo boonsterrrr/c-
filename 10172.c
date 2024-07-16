6b7b19c3d668f4426d5105e3f9613796c29cc4a8
정지현 <boonsterrrr@gmail.com>
2024-07-16 17:07:22 +0900

    10172

---
+++ 10172.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|");
+}
+
+//"를 프린트하고 싶으면 \" 하면 "로 프린트됨.//
