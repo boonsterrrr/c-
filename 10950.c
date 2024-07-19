d57564f04ac38be3fcb737a123307b6f3d50ff2b
정지현 <boonsterrrr@gmail.com>
2024-07-19 16:23:22 +0900

    10950

--- 10950.c
+++ 10950.c
@@ -17,3 +17,22 @@ int main() {
 }
 
 // 쓰읍 이거 생각이 잘 안된다 너무 졸려 내일다시!//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+    int j;
+    int a, b;
+    for (int i = 0; i < t; i++) {
+        scanf("%d %d", &a, &b);
+        j = a + b;
+    }
+    printf("%d\n", j);
+    return 0;
+}
+
