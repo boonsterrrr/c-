cd4f5af295435cf9f3e2fba3daba21b9131f621e
정지현 <boonsterrrr@gmail.com>
2024-07-19 21:49:14 +0900

    영수증

---
+++ 25304.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main() {
+	int x, n, a, b;
+	scanf("%d\n", &x);
+	scanf("%d\n", &n);
+	int sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a, &b);
+		sum += a * b;
+	}
+	if (x == sum) {
+		printf("Yes");
+	}
+	else printf("No");
+
+	
+
+
+}
