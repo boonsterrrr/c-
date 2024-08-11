a123b80f27c2e11c570540c191e86ac318587744
정지현 <boonsterrrr@gmail.com>
2024-08-11 20:12:29 +0900

    잣같은 문제

---
+++ 1402.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int t;
+    int a, b;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        scanf("%d %d", &a, &b);
+        printf("yes\n");
+        
+    }
+
+}
